28af9ff22e324c68544e89ac06c7ef968440a14d
romlive <128592200+romlive@users.noreply.github.com>
2023-03-29 22:03:47 +0300

    add all fund
    add prime, even and odd functions

--- homework_01/main.py
+++ homework_01/main.py
@@ -5,14 +5,17 @@
 
 
 def power_numbers(*numbers):
+    return [num * num for num in numbers]
+
     """
     функция, которая принимает N целых чисел,
     и возвращает список квадратов этих чисел
     >>> power_numbers(1, 2, 5, 7)
     <<< [1, 4, 25, 49]
     """
-    return [number ** 2 for number in numbers]
 
+
+print(power_numbers(1, 2, 5, 7))
 
 # filter types
 ODD = "odd"
@@ -20,7 +23,21 @@ EVEN = "even"
 PRIME = "prime"
 
 
-def filter_numbers(numbers_list, filter_type):
+def is_prime(numbers):
+    results = []
+    for num1 in numbers:
+        if type(num1) is int and num1 != 0:
+            k = True
+            for i in range(2, int(num1 // 2 + 1)):
+                if (num1 % i) == 0:
+                    k = False
+                    break
+            if k and num1 != 1:
+                results.append(num1)
+    return results
+
+
+def filter_numbers(numbers, filter_type):
     """
     функция, которая на вход принимает список из целых чисел,
     и возвращает только чётные/нечётные/простые числа
@@ -31,10 +48,14 @@ def filter_numbers(numbers_list, filter_type):
     >>> filter_numbers([2, 3, 4, 5], EVEN)
     <<< [2, 4]
     """
-    if filter_type == ODD:
-        return [number for number in numbers_list if number % 2 != 0]
+
     if filter_type == EVEN:
-        return [number for number in numbers_list if number % 2 == 0]
+        return list(filter(lambda x: x % 2 == 0, numbers))
+    if filter_type == ODD:
+        return list(filter(lambda x: x % 2 != 0, numbers))
+    if filter_type == PRIME:
+        return is_prime(numbers)
 
 
-
+for num_type in [ODD, EVEN, PRIME]:
+    print(filter_numbers(list(range(0, 20)), num_type))
